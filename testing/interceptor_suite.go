7a285f64f236321949e5c3f1bf06105980dc3022
Dmitry Savintsev <dsavints@verizonmedia.com>
2020-09-07 19:22:22 +0200

    add test certificates generation

--- testing/interceptor_suite.go
+++ testing/interceptor_suite.go
@@ -6,18 +6,28 @@ package grpc_testing
 import (
 	"context"
 	"crypto/tls"
+	"errors"
 	"flag"
+	"html/template"
+	"io/ioutil"
 	"net"
+	"os"
+	"os/exec"
 	"path"
+	"path/filepath"
 	"runtime"
 	"time"
 
-	"github.com/grpc-ecosystem/go-grpc-middleware/testing/certs"
 	pb_testproto "github.com/grpc-ecosystem/go-grpc-middleware/testing/testproto"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+)
+
+const (
+	testCertFilename = "cert.pem"
+	testKeyFilename  = "key.pem"
 )
 
 var (
@@ -48,6 +58,9 @@ type InterceptorTestSuite struct {
 }
 
 func (s *InterceptorTestSuite) SetupSuite() {
+	if err := generateTestCerts(); err != nil {
+		s.T().Fatal("failed to generate test certificate: " + err.Error())
+	}
 	s.restartServerWithDelayedStart = make(chan time.Duration)
 	s.serverRunning = make(chan bool)
 
@@ -60,7 +73,9 @@ func (s *InterceptorTestSuite) SetupSuite() {
 			s.serverAddr = s.ServerListener.Addr().String()
 			require.NoError(s.T(), err, "must be able to allocate a port for serverListener")
 			if *flagTls {
-				localhostCert, err := tls.X509KeyPair(certs.LocalhostCert, certs.LocalhostKey)
+				certFile := path.Join(getTestingCertsPath(), testCertFilename)
+				keyFile := path.Join(getTestingCertsPath(), testKeyFilename)
+				localhostCert, err := tls.LoadX509KeyPair(certFile, keyFile)
 				require.NoError(s.T(), err, "failed loading server credentials for localhostCert")
 				creds := credentials.NewServerTLSFromCert(&localhostCert)
 				s.ServerOpts = append(s.ServerOpts, grpc.Creds(creds))
@@ -105,8 +120,8 @@ func (s *InterceptorTestSuite) NewClient(dialOpts ...grpc.DialOption) pb_testpro
 	newDialOpts := append(dialOpts, grpc.WithBlock())
 	if *flagTls {
 		creds, err := credentials.NewClientTLSFromFile(
-			path.Join(getTestingCertsPath(), "localhost.crt"), "localhost")
-		require.NoError(s.T(), err, "failed reading client credentials for localhost.crt")
+			path.Join(getTestingCertsPath(), testCertFilename), "localhost")
+		require.NoError(s.T(), err, "failed reading client credentials for "+testCertFilename)
 		newDialOpts = append(newDialOpts, grpc.WithTransportCredentials(creds))
 	} else {
 		newDialOpts = append(newDialOpts, grpc.WithInsecure())
@@ -142,4 +157,66 @@ func (s *InterceptorTestSuite) TearDownSuite() {
 	if s.clientConn != nil {
 		s.clientConn.Close()
 	}
+}
+
+type keycert struct {
+	Key, Cert string
+}
+
+func generateTestCerts() error {
+	oldDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	newDir := getTestingCertsPath()
+	if newDir == "" {
+		return errors.New("error generating test certificates - unable to get testing certs path")
+	}
+	if err := os.Chdir(newDir); err != nil {
+		return errors.New("error generating test certificates - unable to chdir to " + newDir)
+	}
+
+	defer os.Chdir(oldDir)
+	goroot := runtime.GOROOT()
+	cmd := exec.Command(filepath.Join(goroot, "bin/go"),
+		"run",
+		filepath.Join(goroot, "src/crypto/tls/generate_cert.go"),
+		"--ca",
+		"--rsa-bits", "2048",
+		"--host", "localhost,example.com",
+		"--start-date", "Jan 1 00:00:00 2020",
+		"--duration=1000000h")
+	if _, err := cmd.CombinedOutput(); err != nil {
+		return err
+	}
+	keyBytes, err := ioutil.ReadFile("key.pem")
+	if err != nil {
+		return err
+	}
+	certBytes, err := ioutil.ReadFile("cert.pem")
+	if err != nil {
+		return err
+	}
+	data := keycert{
+		Key:  string(keyBytes),
+		Cert: string(certBytes),
+	}
+	// localhost.go template
+	const tmpl = `package certs
+
+var LocalhostKey = []byte(` + "`" + `{{.Key}}` + "`" + `)
+
+var LocalhostCert = []byte(` + "`" + `{{.Cert}}` + "`" + `)
+`
+
+	t := template.Must(template.New("keycert-go").Parse(tmpl))
+	f, err := os.OpenFile("localhost.go", os.O_RDWR|os.O_TRUNC|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if err := t.Execute(f, data); err != nil {
+		return err
+	}
+	return nil
 }
